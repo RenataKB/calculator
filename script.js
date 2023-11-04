29779fcb4a55c053045a4b5e4049371ee185069b
Renata <renata.kb@gmail.com>
2023-11-04 16:47:13 -0300

    Add script with main operations

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// TODO
+add = (n1, n2) => n1 + n2;
+substract = (n1, n2) => n1 - n2;
+multiply = (n1, n2) => n1 * n2;
+divide = (n1, n2) => n1 / n2;
+
+let n1, n2, op;
+
+function operate(op, n1, n2) {
+    switch (op) {
+        case '+':
+            return add(n1, n2);
+        case '-':
+            return substract(n1, n2);
+        case '*':
+            return multiply(n1, n2);
+        case '/':
+            return divide(n1, n2);
+        default:
+            return;
+    }
+}
